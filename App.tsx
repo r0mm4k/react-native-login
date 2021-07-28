b4316e70d7509ac0f8354a08db09f1912169b56b
Roma Makarov <r0mmm4k@gmail.com>
2021-07-28 11:54:37 +0300

    - example configure transition between screens

--- App.tsx
+++ App.tsx
@@ -1,40 +1,58 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from '@react-navigation/stack';
 
-import { Button, Header, TextInput } from './src/components';
+import { Button, Header } from './src/components';
 import { theme } from './src/core';
 
-const Stack = createStackNavigator();
+type TRootStackParamList = {
+  Screen1: undefined;
+  Screen2: undefined;
+};
 
-const Screen1 = () => {
+type TScreen1ScreenNavigationProp = StackNavigationProp<
+  TRootStackParamList,
+  'Screen1'
+>;
+
+type TScreen2ScreenNavigationProp = StackNavigationProp<
+  TRootStackParamList,
+  'Screen2'
+>;
+
+interface IScreen1 {
+  navigation: TScreen1ScreenNavigationProp;
+}
+
+interface IScreen2 {
+  navigation: TScreen2ScreenNavigationProp;
+}
+
+const RootStack = createStackNavigator<TRootStackParamList>();
+
+const Screen1: FC<IScreen1> = ({ navigation }) => {
+  const showScreen2 = () => navigation.navigate('Screen2');
+
   return (
     <View style={styles.container}>
-      <Header>Open!</Header>
-      <Button mode="contained">Click me!</Button>
-      <TextInput label="email" />
+      <Header>Screen1</Header>
+      <Button onPress={showScreen2}>Go To Second Screen</Button>
     </View>
   );
 };
 
-const Screen2 = () => {
-  return (
-    <View style={styles.container}>
-      <Header>Open!</Header>
-      <Button mode="contained">Click me!</Button>
-      <TextInput label="email" />
-    </View>
-  );
-};
+const Screen2: FC<IScreen2> = ({ navigation }) => {
+  const goBack = () => navigation.goBack();
 
-const Screen3 = () => {
   return (
     <View style={styles.container}>
-      <Header>Open!</Header>
-      <Button mode="contained">Click me!</Button>
-      <TextInput label="email" />
+      <Header>Screen2</Header>
+      <Button onPress={goBack}>Go Back</Button>
     </View>
   );
 };
@@ -43,14 +61,13 @@ export default function App() {
   return (
     <Provider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator
+        <RootStack.Navigator
           initialRouteName="Screen1"
           screenOptions={{ headerShown: false }}
         >
-          <Stack.Screen name="Screen1" component={Screen1} />
-          <Stack.Screen name="Screen2" component={Screen2} />
-          <Stack.Screen name="Screen3" component={Screen3} />
-        </Stack.Navigator>
+          <RootStack.Screen name="Screen1" component={Screen1} />
+          <RootStack.Screen name="Screen2" component={Screen2} />
+        </RootStack.Navigator>
       </NavigationContainer>
     </Provider>
   );
