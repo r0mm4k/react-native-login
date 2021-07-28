f1430dfd1c4e04b8a87dc3fec161158d9d3913fb
Roma Makarov <r0mmm4k@gmail.com>
2021-07-28 10:33:11 +0300

    - init  - configure eslint & prettier  - add ui lib

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Open!</Text>
+      </View>
+    </Provider>
   );
 }
 
