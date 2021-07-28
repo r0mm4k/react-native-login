ce58e3325b379d8422cb633557b51b2f873a4306
Roma Makarov <r0mmm4k@gmail.com>
2021-07-28 11:34:00 +0300

    - add & configure react navigation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,57 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { Button, Header, TextInput } from './src/components';
 import { theme } from './src/core';
 
+const Stack = createStackNavigator();
+
+const Screen1 = () => {
+  return (
+    <View style={styles.container}>
+      <Header>Open!</Header>
+      <Button mode="contained">Click me!</Button>
+      <TextInput label="email" />
+    </View>
+  );
+};
+
+const Screen2 = () => {
+  return (
+    <View style={styles.container}>
+      <Header>Open!</Header>
+      <Button mode="contained">Click me!</Button>
+      <TextInput label="email" />
+    </View>
+  );
+};
+
+const Screen3 = () => {
+  return (
+    <View style={styles.container}>
+      <Header>Open!</Header>
+      <Button mode="contained">Click me!</Button>
+      <TextInput label="email" />
+    </View>
+  );
+};
+
 export default function App() {
   return (
     <Provider theme={theme}>
-      <View style={styles.container}>
-        <Header>Open!</Header>
-        <Button mode="contained">Click me!</Button>
-        <TextInput label="email" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Screen1"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Screen1" component={Screen1} />
+          <Stack.Screen name="Screen2" component={Screen2} />
+          <Stack.Screen name="Screen3" component={Screen3} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
